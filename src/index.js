7d9a678e33b8b852867938565d45a6f00d0121ef
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-25 10:19:39 -0300

    config redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import storeConfig from './store/storeConfig';
+
+const store = storeConfig()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+<React.StrictMode>
+    <App />
+
+</React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/store/storeConfig.js
@@ -0,0 +1,24 @@
+import {createStore, combineReducers} from 'redux'
+/* import numbersReducer from './reducers/numbers.js'
+ */
+const reducers = combineReducers({
+    numeros: function(state,action){
+        return{
+            min: 1,
+            max: 10
+        }
+    },
+    nomes: function (state,action) {
+        return[
+            'Ana',
+            'Amor',
+            'da minha vida'
+        ]
+    }
+}) 
+
+ function storeConfig() {
+    return createStore(reducers)
+}
+
+export default storeConfig  
