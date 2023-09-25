5e35932f4d21d8b2ad4eb6e9276d29982edd3e21
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-25 10:44:13 -0300

    add intervalo component using redux

--- src/components/intervalo.jsx
+++ src/components/intervalo.jsx
@@ -3,26 +3,33 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Card from './Card'
-/* import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numbers'
-
+/* /import { alterarNumeroMinimo, alterarNumeroMaximo } from '../store/actions/numbers'
+ */
 function Intervalo(props) {
     const { min, max } = props
-    // props.alterarMinimo(10000)
- */ 
-export default props=>{
 
     return (
         <Card title="Intervalo de números" red>
             <div className='Intervalo'>
                 <span>
                     <strong>Mínimo: </strong>
-                    <input type="number" value={0} readOnly /* onChange={e => props.alterarMinimo(+e.target.value)} */ />
+                    <input type="number" value={min} readOnly /* onChange={e => props.alterarMinimo(+e.target.value)} */ />
                 </span>
                 <span>
                     <strong>Máximo: </strong>
-                    <input type="number" value={10} readOnly /* onChange={e => props.alterarMaximo(+e.target.value)}  *//>
+                    <input type="number" value={max} readOnly /* onChange={e => props.alterarMaximo(+e.target.value)}  *//>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProsps (state){
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max,
+
+    }
+}
+
+export default connect (mapStateToProsps) (Intervalo)
