d817a0233b411f7966a0a590dc3b7267a3e778b9
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-25 10:35:30 -0300

    add Soma component using redux

--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -2,17 +2,31 @@
 import React from 'react'
 
 import Card from './Card'
+import { connect } from 'react-redux'
 
-export default props=>{
-
+function Soma(props){
+const {min, max} = props
     return (
         <Card title="Soma do números" blue>
             <div >
                 <span>
                     <span>Resultado: </span>
-                    <strong>10</strong>
+                    <strong>{min+max}</strong>
                 </span>
             </div>
         </Card>
     )
+
 }
+    function mapStateToProsps (state){
+        return{
+            min: state.numeros.min,
+            max: state.numeros.max,
+    
+        }
+    }
+    
+
+
+
+export default connect(mapStateToProsps) (Soma)
