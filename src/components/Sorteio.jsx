6846173a0ac27c13be55a8a1339486ef3ea4d141
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-25 10:43:58 -0300

    add Sorteio component using redux

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
 import Card from './Card'
+import { connect } from 'react-redux'
 
-export default props=>{
-
+ function Sorteio(props){
+    const{min,max}=props
+    const aleatorio =  parseInt(Math.random()* (max-min)) +min
     return (
         <Card title="Sorteio do números" purple>
             <div >
                 <span>
                     <span>Resultado: </span>
-                    <strong>3</strong>
+                    <strong>{aleatorio}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+
+function mapStateToProsps (state){
+    return{
+        min: state.numeros.min,
+        max: state.numeros.max,
+
+    }
+}
+
+export default connect(mapStateToProsps) (Sorteio)
+
+
